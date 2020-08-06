fa49cdc4a0693591620b71b0ca7a4ca4f4dea37e
Jdevents <icyztv@gmail.com>
2020-08-06 11:30:19 +1000

    Finished Moving everything over to git

---
+++ Convert/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+
+namespace Convert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "MP3 File (*.mp3)|*.mp3;";
+            if (open.ShowDialog() != DialogResult.OK) return;
+
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "WAV File (*.wav)|*.wav;";
+            if (save.ShowDialog() != DialogResult.OK) return;
+
+            using (Mp3FileReader mp3 = new Mp3FileReader(open.FileName))
+            {
+                using (WaveStream pcm = WaveFormatConversionStream.CreatePcmStream(mp3))
+                {
+                    WaveFileWriter.CreateWaveFile(save.FileName, pcm);
+                    MessageBox.Show("Done Converting", "DONE!");
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {          
+            MessageBox.Show("Can't convet Long Videos. Please Only use 0:50 sec to 3:00 Mins");
+        }
+    }
+}
