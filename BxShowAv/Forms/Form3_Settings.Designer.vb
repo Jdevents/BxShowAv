e9b8ba9371790a94d3cfe5f7a01a5bbd65fcb942
Harrison Davis <33649239+Jdevents@users.noreply.github.com>
2021-02-26 15:41:16 +1100

    Finished working on the button matrix
    Change log:
       - Fixed up GUI for matrix settings
       - Fixed audio path not being set
       - Fixed Name setting

--- BxShowAv/Forms/Form3_Settings.Designer.vb
+++ BxShowAv/Forms/Form3_Settings.Designer.vb
@@ -32,16 +32,26 @@ Partial Class Form3_Settings
         Me.SongNameTextEdit = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.NameGet = New AxWMPLib.AxWindowsMediaPlayer()
-        Me.SimpleButton3 = New DevExpress.XtraEditors.SimpleButton()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.SimpleButton4 = New DevExpress.XtraEditors.SimpleButton()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.songpath_lbl = New System.Windows.Forms.Label()
+        Me.GroupControl1 = New DevExpress.XtraEditors.GroupControl()
+        Me.Songtot = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.SeparatorControl1 = New DevExpress.XtraEditors.SeparatorControl()
+        Me.Songtime = New System.Windows.Forms.Timer(Me.components)
+        Me.SimpleButton5 = New DevExpress.XtraEditors.SimpleButton()
+        Me.SimpleButton7 = New DevExpress.XtraEditors.SimpleButton()
         CType(Me.songpath.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.BehaviorManager1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.NameGet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GroupControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupControl1.SuspendLayout()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SeparatorControl1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
@@ -59,7 +69,7 @@ Partial Class Form3_Settings
         Me.SimpleButton1.ImageOptions.Image = CType(resources.GetObject("SimpleButton1.ImageOptions.Image"), System.Drawing.Image)
         Me.SimpleButton1.Location = New System.Drawing.Point(14, 102)
         Me.SimpleButton1.Name = "SimpleButton1"
-        Me.SimpleButton1.Size = New System.Drawing.Size(75, 23)
+        Me.SimpleButton1.Size = New System.Drawing.Size(60, 23)
         Me.SimpleButton1.TabIndex = 1
         Me.SimpleButton1.Text = "Apply"
         '
@@ -67,9 +77,9 @@ Partial Class Form3_Settings
         '
         Me.SimpleButton2.DialogResult = System.Windows.Forms.DialogResult.Cancel
         Me.SimpleButton2.ImageOptions.Image = CType(resources.GetObject("SimpleButton2.ImageOptions.Image"), System.Drawing.Image)
-        Me.SimpleButton2.Location = New System.Drawing.Point(95, 102)
+        Me.SimpleButton2.Location = New System.Drawing.Point(80, 102)
         Me.SimpleButton2.Name = "SimpleButton2"
-        Me.SimpleButton2.Size = New System.Drawing.Size(75, 23)
+        Me.SimpleButton2.Size = New System.Drawing.Size(60, 23)
         Me.SimpleButton2.TabIndex = 2
         Me.SimpleButton2.Text = "Exit"
         '
@@ -83,7 +93,7 @@ Partial Class Form3_Settings
         Me.songpath.Properties.Appearance.Options.UseFont = True
         Me.songpath.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton()})
         Me.songpath.Properties.ReadOnly = True
-        Me.songpath.Size = New System.Drawing.Size(542, 26)
+        Me.songpath.Size = New System.Drawing.Size(768, 26)
         Me.songpath.TabIndex = 4
         '
         'SongNameTextEdit
@@ -91,7 +101,7 @@ Partial Class Form3_Settings
         Me.SongNameTextEdit.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.SongNameTextEdit.Location = New System.Drawing.Point(138, 44)
         Me.SongNameTextEdit.Name = "SongNameTextEdit"
-        Me.SongNameTextEdit.Size = New System.Drawing.Size(518, 27)
+        Me.SongNameTextEdit.Size = New System.Drawing.Size(744, 27)
         Me.SongNameTextEdit.TabIndex = 29
         '
         'Label2
@@ -107,25 +117,17 @@ Partial Class Form3_Settings
         'NameGet
         '
         Me.NameGet.Enabled = True
-        Me.NameGet.Location = New System.Drawing.Point(573, 102)
+        Me.NameGet.Location = New System.Drawing.Point(146, 99)
         Me.NameGet.Name = "NameGet"
         Me.NameGet.OcxState = CType(resources.GetObject("NameGet.OcxState"), System.Windows.Forms.AxHost.State)
         Me.NameGet.Size = New System.Drawing.Size(95, 24)
         Me.NameGet.TabIndex = 33
-        '
-        'SimpleButton3
-        '
-        Me.SimpleButton3.ImageOptions.Image = CType(resources.GetObject("SimpleButton3.ImageOptions.Image"), System.Drawing.Image)
-        Me.SimpleButton3.Location = New System.Drawing.Point(314, 103)
-        Me.SimpleButton3.Name = "SimpleButton3"
-        Me.SimpleButton3.Size = New System.Drawing.Size(81, 23)
-        Me.SimpleButton3.TabIndex = 34
-        Me.SimpleButton3.Text = "Test Audio"
+        Me.NameGet.Visible = False
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(11, 71)
+        Me.Label4.Location = New System.Drawing.Point(11, 78)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(76, 13)
         Me.Label4.TabIndex = 35
@@ -134,7 +136,7 @@ Partial Class Form3_Settings
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(92, 71)
+        Me.Label5.Location = New System.Drawing.Point(92, 78)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(32, 13)
         Me.Label5.TabIndex = 36
@@ -143,25 +145,16 @@ Partial Class Form3_Settings
         'SimpleButton4
         '
         Me.SimpleButton4.ImageOptions.Image = CType(resources.GetObject("SimpleButton4.ImageOptions.Image"), System.Drawing.Image)
-        Me.SimpleButton4.Location = New System.Drawing.Point(176, 102)
+        Me.SimpleButton4.Location = New System.Drawing.Point(74, 129)
         Me.SimpleButton4.Name = "SimpleButton4"
         Me.SimpleButton4.Size = New System.Drawing.Size(132, 23)
         Me.SimpleButton4.TabIndex = 37
         Me.SimpleButton4.Text = "Set Name To Original"
         '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(443, 103)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 38
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(12, 86)
+        Me.Label3.Location = New System.Drawing.Point(11, 160)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(63, 13)
         Me.Label3.TabIndex = 39
@@ -170,25 +163,102 @@ Partial Class Form3_Settings
         'songpath_lbl
         '
         Me.songpath_lbl.AutoSize = True
-        Me.songpath_lbl.Location = New System.Drawing.Point(92, 86)
+        Me.songpath_lbl.Location = New System.Drawing.Point(91, 160)
         Me.songpath_lbl.Name = "songpath_lbl"
         Me.songpath_lbl.Size = New System.Drawing.Size(44, 13)
         Me.songpath_lbl.TabIndex = 40
         Me.songpath_lbl.Text = "Nothing"
+        '
+        'GroupControl1
+        '
+        Me.GroupControl1.Controls.Add(Me.Songtot)
+        Me.GroupControl1.Controls.Add(Me.Label6)
+        Me.GroupControl1.Controls.Add(Me.TrackBar1)
+        Me.GroupControl1.Controls.Add(Me.SeparatorControl1)
+        Me.GroupControl1.Location = New System.Drawing.Point(212, 77)
+        Me.GroupControl1.Name = "GroupControl1"
+        Me.GroupControl1.Size = New System.Drawing.Size(670, 80)
+        Me.GroupControl1.TabIndex = 41
+        '
+        'Songtot
+        '
+        Me.Songtot.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Songtot.AutoSize = True
+        Me.Songtot.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Songtot.Location = New System.Drawing.Point(577, 52)
+        Me.Songtot.Name = "Songtot"
+        Me.Songtot.Size = New System.Drawing.Size(88, 18)
+        Me.Songtot.TabIndex = 45
+        Me.Songtot.Text = "00:00/00:00"
+        '
+        'Label6
+        '
+        Me.Label6.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(577, 29)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(83, 18)
+        Me.Label6.TabIndex = 43
+        Me.Label6.Text = "Song Time:"
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.TrackBar1.Location = New System.Drawing.Point(5, 30)
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(548, 45)
+        Me.TrackBar1.TabIndex = 42
+        Me.TrackBar1.TickStyle = System.Windows.Forms.TickStyle.Both
+        '
+        'SeparatorControl1
+        '
+        Me.SeparatorControl1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.SeparatorControl1.AutoSizeMode = True
+        Me.SeparatorControl1.LineColor = System.Drawing.Color.Black
+        Me.SeparatorControl1.LineOrientation = System.Windows.Forms.Orientation.Vertical
+        Me.SeparatorControl1.Location = New System.Drawing.Point(559, 27)
+        Me.SeparatorControl1.Name = "SeparatorControl1"
+        Me.SeparatorControl1.Size = New System.Drawing.Size(19, 50)
+        Me.SeparatorControl1.TabIndex = 44
+        '
+        'Songtime
+        '
+        '
+        'SimpleButton5
+        '
+        Me.SimpleButton5.ImageOptions.Image = CType(resources.GetObject("SimpleButton5.ImageOptions.Image"), System.Drawing.Image)
+        Me.SimpleButton5.Location = New System.Drawing.Point(14, 129)
+        Me.SimpleButton5.Name = "SimpleButton5"
+        Me.SimpleButton5.Size = New System.Drawing.Size(24, 23)
+        Me.SimpleButton5.TabIndex = 42
+        '
+        'SimpleButton7
+        '
+        Me.SimpleButton7.ImageOptions.Image = CType(resources.GetObject("SimpleButton7.ImageOptions.Image"), System.Drawing.Image)
+        Me.SimpleButton7.Location = New System.Drawing.Point(44, 129)
+        Me.SimpleButton7.Name = "SimpleButton7"
+        Me.SimpleButton7.Size = New System.Drawing.Size(24, 23)
+        Me.SimpleButton7.TabIndex = 44
         '
         'Form3_Settings
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.CancelButton = Me.SimpleButton2
-        Me.ClientSize = New System.Drawing.Size(668, 137)
+        Me.ClientSize = New System.Drawing.Size(894, 178)
+        Me.Controls.Add(Me.SimpleButton7)
+        Me.Controls.Add(Me.SimpleButton5)
+        Me.Controls.Add(Me.GroupControl1)
         Me.Controls.Add(Me.songpath_lbl)
         Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.SimpleButton4)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.SimpleButton3)
         Me.Controls.Add(Me.NameGet)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.SongNameTextEdit)
@@ -198,12 +268,19 @@ Partial Class Form3_Settings
         Me.Controls.Add(Me.Label1)
         Me.IconOptions.Image = CType(resources.GetObject("Form3_Settings.IconOptions.Image"), System.Drawing.Image)
         Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(894, 208)
         Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(894, 208)
         Me.Name = "Form3_Settings"
         Me.Text = "Add / Edit Sound Path"
         CType(Me.songpath.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.BehaviorManager1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.NameGet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GroupControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupControl1.ResumeLayout(False)
+        Me.GroupControl1.PerformLayout()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SeparatorControl1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -217,11 +294,17 @@ Partial Class Form3_Settings
     Friend WithEvents SongNameTextEdit As TextBox
     Friend WithEvents Label2 As Label
     Friend WithEvents NameGet As AxWMPLib.AxWindowsMediaPlayer
-    Friend WithEvents SimpleButton3 As DevExpress.XtraEditors.SimpleButton
     Friend WithEvents Label4 As Label
     Friend WithEvents Label5 As Label
     Friend WithEvents SimpleButton4 As DevExpress.XtraEditors.SimpleButton
-    Friend WithEvents Button1 As Button
     Friend WithEvents Label3 As Label
     Friend WithEvents songpath_lbl As Label
+    Friend WithEvents GroupControl1 As DevExpress.XtraEditors.GroupControl
+    Friend WithEvents Songtime As Timer
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents Label6 As Label
+    Friend WithEvents SeparatorControl1 As DevExpress.XtraEditors.SeparatorControl
+    Friend WithEvents Songtot As Label
+    Friend WithEvents SimpleButton5 As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents SimpleButton7 As DevExpress.XtraEditors.SimpleButton
 End Class
--- BxShowAv/Forms/Form3_Settings.vb
+++ BxShowAv/Forms/Form3_Settings.vb
@@ -19,45 +19,27 @@ Public Class Form3_Settings
 
     Private Sub songpath_EditValueChanged(sender As Object, e As EventArgs) Handles songpath.EditValueChanged
 
-        If Path = Nothing Then
-            'do nothing
-        ElseIf songpath.Text = Path Then
-            If SongNameTextEdit.Text = Songname Then
+        If songpath.Text = Path Then
+            If Path = "" Then
+                Try
+                    songpath.Text = Path
+                Catch ex As Exception
 
-                NameGet.URL = songpath.Text
-                NameGet.Ctlcontrols.stop()
-                SongNameTextEdit.Text = NameGet.currentMedia.name
-                Label5.Text = "True"
-                Label5.ForeColor = Color.Green
-                SimpleButton4.Enabled = True
-                SimpleButton3.Enabled = True
-
-            Else
-
-                NameGet.URL = songpath.Text
-                NameGet.Ctlcontrols.stop()
-                Label5.Text = "True"
-                Label5.ForeColor = Color.Green
-                SimpleButton4.Enabled = True
-                SimpleButton3.Enabled = True
-
+                End Try
             End If
+        Else
+            Try
+                NameGet.URL = songpath.Text
+                NameGet.settings.autoStart = False
+                SongNameTextEdit.Text = NameGet.currentMedia.name
+                songpath_lbl.Text = songpath.Text
+                Label5.Text = "True"
+                Label5.ForeColor = Color.Green
+                SimpleButton4.Enabled = True
+            Catch ex As Exception
+                ' MsgBox(ex.Message)
+            End Try
         End If
-    End Sub
-
-    Private Sub SimpleButton3_Click(sender As Object, e As EventArgs) Handles SimpleButton3.Click
-        NameGet.settings.volume = 100
-        NameGet.URL = songpath.Text
-
-        If NameGet.playState = WMPLib.WMPPlayState.wmppsPlaying Then
-            NameGet.Ctlcontrols.stop()
-            NameGet.settings.volume = 0
-        ElseIf NameGet.playState = WMPLib.WMPPlayState.wmppsStopped Then
-            NameGet.URL = songpath.Text
-            NameGet.Ctlcontrols.play()
-            NameGet.settings.volume = 100
-        End If
-
     End Sub
 
     Private Sub namegetplayer_CurrentItemChange(sender As Object, e As AxWMPLib._WMPOCXEvents_CurrentItemChangeEvent) Handles NameGet.CurrentItemChange
@@ -67,13 +49,13 @@ Public Class Form3_Settings
             Label5.Text = "True"
             Label5.ForeColor = Color.Green
             SimpleButton4.Enabled = True
-            SimpleButton3.Enabled = True
             SongNameTextEdit.Text = NameGet.currentMedia.name
         End If
     End Sub
     Private Sub Form3_Settings_Load(sender As Object, e As EventArgs) Handles Me.Load
         NameGet.settings.autoStart = False
         SongNameTextEdit.Text = Songname
+        songpath.Text = Path
         songpath_lbl.Text = NameGet.URL.ToString
         Try
             NameGet.URL = val.Path
@@ -85,12 +67,10 @@ Public Class Form3_Settings
             Label5.Text = "No Audio Loaded"
             Label5.ForeColor = Color.Red
             SimpleButton4.Enabled = False
-            SimpleButton3.Enabled = False
         Else
             Label5.Text = "Audio Loaded"
             Label5.ForeColor = Color.Green
             SimpleButton4.Enabled = True
-            SimpleButton3.Enabled = True
         End If
 
     End Sub
@@ -99,14 +79,45 @@ Public Class Form3_Settings
         SongNameTextEdit.Text = NameGet.currentMedia.name
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MsgBox(NameGet.URL.ToString)
-    End Sub
-
     Private Sub Form3_Settings_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
         If NameGet.playState = WMPLib.WMPPlayState.wmppsPlaying Then
             NameGet.Ctlcontrols.stop()
             NameGet.settings.volume = 0
         End If
+    End Sub
+
+    Private Sub NameGet_OpenStateChange(sender As Object, e As AxWMPLib._WMPOCXEvents_OpenStateChangeEvent) Handles NameGet.OpenStateChange
+        If NameGet.openState = WMPLib.WMPOpenState.wmposMediaOpen Then
+            TrackBar1.Maximum = CInt(NameGet.currentMedia.duration)
+            Songtime.Start()
+        End If
+    End Sub
+
+    Private Sub Songtime_Tick(sender As Object, e As EventArgs) Handles Songtime.Tick
+        Try
+            TrackBar1.Value = CInt(Me.NameGet.Ctlcontrols.currentPosition)
+            GroupControl1.Text = "Song Time: " & NameGet.Ctlcontrols.currentPositionString & "/" & NameGet.currentMedia.durationString
+            Songtot.Text = NameGet.Ctlcontrols.currentPositionString & "/" & NameGet.currentMedia.durationString
+        Catch ex As Exception
+            'This is need because it gets the shits if you close the video
+            'so it just resets the text-box text to nothing
+            Songtot.Text = "00:00/00:00"
+        End Try
+    End Sub
+
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+        NameGet.Ctlcontrols.pause()
+        NameGet.Ctlcontrols.currentPosition = CDbl(TrackBar1.Value)
+        System.Threading.Thread.Sleep(3)
+        NameGet.Ctlcontrols.play()
+    End Sub
+
+    Private Sub SimpleButton5_Click(sender As Object, e As EventArgs) Handles SimpleButton5.Click
+        NameGet.settings.volume = 100
+        NameGet.Ctlcontrols.play()
+    End Sub
+
+    Private Sub SimpleButton7_Click(sender As Object, e As EventArgs) Handles SimpleButton7.Click
+        NameGet.Ctlcontrols.pause()
     End Sub
 End Class
